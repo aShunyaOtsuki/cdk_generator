41a24a656fea62d57f7aa8a3b182162abd914e56
aShunyaOtuski <shunya.otsuki@access-company.com>
2021-08-17 04:43:29 +0900

    fix: use readFilesync

--- md2cdk.ts
+++ md2cdk.ts
@@ -1,11 +1,6 @@
 import * as fs from "fs"
 
-fs.readFile('./table.md', "utf-8", (err, file) => {
-  if (err) {
-    console.error(err);
-    process.exit(1);
-  }
-  console.log(file); // read table.md as string
-  const output = "console.log('test')"
-  fs.writeFileSync('./cdkSample/lib/DynamoDBStack.ts', output)
-})
+const file = fs.readFileSync('./table.md', "utf-8")
+console.log(file); // read table.md as string
+const output = "console.log('test')"
+fs.writeFileSync('./cdkSample/lib/DynamoDBStack.ts', output)
