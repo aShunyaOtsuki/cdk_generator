676735e8c4ab4a7344c4b06733df80f8380a53fe
aShunyaOtuski <shunya.otsuki@access-company.com>
2021-08-17 05:41:02 +0900

    imp: file to table formatter

--- md2cdk.ts
+++ md2cdk.ts
@@ -1,6 +1,25 @@
 import * as fs from "fs"
 
-const file = fs.readFileSync('./table.md', "utf-8")
-console.log(file); // read table.md as string
+const generateType = (file: string[]): object[] => {
+  const index = file.findIndex((r) => r.startsWith('|---|')) + 1
+  const type = {}
+  const key = {}
+  for (let i = index; i < file.length; i++) {
+    const row = file[i].split('|')
+    type[row[1]] = row[2]
+    if (row[3] === 'yes') {
+      key[row[1]] = row[2]
+    }
+  }
+  return [key, type]
+}
+
+const file = fs.readFileSync('./table.md', "utf-8").split('\n')
+console.log(file)
+const [key, type] = generateType(file)
+const table = {
+  title: file[0].replace('##', '').split(' ').join(''), key, type,
+}
+console.log(table)
 const output = "console.log('test')"
 fs.writeFileSync('./cdkSample/lib/DynamoDBStack.ts', output)
